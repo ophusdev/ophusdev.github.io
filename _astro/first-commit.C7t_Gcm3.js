2925c3b98ca796d12a925b000f059d5cdf817626
ophusdev <ophusdev@gmail.com>
2024-03-16 15:42:20 +0100

    remove old static files

--- _astro/first-commit.C7t_Gcm3.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["_astro/first-commit.BDGLicJM.js","_astro/hoisted.BgruGIU6.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as t}from"./hoisted.BgruGIU6.js";async function o(){return t(()=>import("./first-commit.BDGLicJM.js"),__vite__mapDeps([0,1]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
