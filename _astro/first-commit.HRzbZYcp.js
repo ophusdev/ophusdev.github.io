446e37b35ba80249e1f138d2f0ce63c06ecefc29
ophusdev <ophusdev@gmail.com>
2024-03-16 15:42:41 +0100

    add TwelveFactor post

---
+++ _astro/first-commit.HRzbZYcp.js
@@ -0,0 +1,11 @@
+import{c as o,r as n,m as s}from"./render-template.D4TDICqz.js";import{u}from"./hoisted.BkfrAwNe.js";const e=`<h2 id="qualche-info-generale-sul-setup">Qualche info generale sul setup</h2>
+<p>Il sito è costruito con Astro 4.0.</p>
+<p>Il sito è hostato sulle Github Pages e dployato usando le Github Actions
+ogni volta che viene pushato un commit su master</p>`,a={title:"First commit",description:"Setup del sito e deploy",publishDate:"10 Mar 2024",updatedDate:"10 Mar 2024",tags:[],minutesRead:"1 min read"},i="/home/io/Desktop/ophusdev-blog/src/content/post/first-commit.md",l=void 0;function f(){return`
+## Qualche info generale sul setup
+
+Il sito è costruito con Astro 4.0.
+
+Il sito è hostato sulle Github Pages e dployato usando le Github Actions
+ogni volta che viene pushato un commit su master
+`}function g(){return e}function b(){return[{depth:2,slug:"qualche-info-generale-sul-setup",text:"Qualche info generale sul setup"}]}const v=o((r,c,p)=>{const{layout:m,...t}=a;return t.file=i,t.url=l,n`${s()}${u(e)}`});export{v as Content,g as compiledContent,v as default,i as file,a as frontmatter,b as getHeadings,f as rawContent,l as url};
---
+++ _astro/twelve-factor.2Lj83UJY.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["_astro/twelve-factor.V6_t_0Ie.js","_astro/render-template.D4TDICqz.js","_astro/hoisted.BkfrAwNe.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as t}from"./hoisted.BkfrAwNe.js";async function o(){return t(()=>import("./twelve-factor.V6_t_0Ie.js"),__vite__mapDeps([0,1,2]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
