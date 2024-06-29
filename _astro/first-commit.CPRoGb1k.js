f1cec2ee3990982d2a76046cce9758802010b803
ophusdev <ophusdev@gmail.com>
2024-06-29 15:38:52 +0200

    remove old static files

--- _astro/first-commit.CPRoGb1k.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["_astro/first-commit.Dz9kbA_w.js","_astro/render-template.BSTUbHJp.js","_astro/astro/assets-service.CWtZdUDo.js","_astro/hoisted.GXsh7Kbe.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as t}from"./astro/assets-service.CWtZdUDo.js";async function o(){return t(()=>import("./first-commit.Dz9kbA_w.js"),__vite__mapDeps([0,1,2,3]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
--- _astro/go-weather.DxuadsGv.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["_astro/go-weather.BitMDpGC.js","_astro/hoisted.GXsh7Kbe.js","_astro/astro/assets-service.CWtZdUDo.js","_astro/render-template.BSTUbHJp.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as t}from"./astro/assets-service.CWtZdUDo.js";async function o(){return t(()=>import("./go-weather.BitMDpGC.js"),__vite__mapDeps([0,1,2,3]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
