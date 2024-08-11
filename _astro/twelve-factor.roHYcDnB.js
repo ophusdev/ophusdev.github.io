7f78d426398e6596989eefe9318de0759302669a
ophusdev <ophusdev@gmail.com>
2024-08-11 18:21:44 +0200

    remove old static files

--- _astro/twelve-factor.roHYcDnB.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["_astro/twelve-factor.Cg9SQzDL.js","_astro/render-template.CwnOUZ39.js","_astro/astro/assets-service.UPL31UwL.js","_astro/hoisted.BC66ynFj.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as t}from"./astro/assets-service.UPL31UwL.js";async function o(){return t(()=>import("./twelve-factor.Cg9SQzDL.js"),__vite__mapDeps([0,1,2,3]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
